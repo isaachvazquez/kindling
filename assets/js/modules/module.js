0f08917bf0bb53ec4296de951e0269513f26ca2b
Isaac Vazquez <ihvazquez@Isaacs-MacBook-Pro.local>
2016-02-10 11:45:05 -0600

    Add scss and js directories.

---
+++ assets/js/modules/module.js
@@ -0,0 +1,16 @@
+// Module
+var a = 0,
+  b = 200;
+
+
+if(a === b) {
+  console.log('True dat.');
+}
+
+for(var i=0; i<b; i++) {
+  console.log('Arrrgggggghhhhh I am a pirate');
+}
+
+var x = 212;
+var z = 23;
+var y = 2;
