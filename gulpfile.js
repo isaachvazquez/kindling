aaf0c89d6ed7620f4be4096557256c251fcb4637
Isaac Vazquez <isaachvazquez@gmail.com>
2016-09-21 14:49:08 -0500

    Add 'test' gulp task.
    - Add gulp-tape and tap-colorize to dependency list

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 // =====================================================================
 // Gulp Tasks
 // Install Gulp plugins:
-// npm install --save-dev gulp-uglify gulp-sass gulp-concat gulp-cssnano
+// npm install --save-dev gulp-uglify gulp-sass gulp-concat gulp-cssnano gulp-tape tap-colorize
 // =====================================================================
 var gulp = require('gulp'),
 		uglify = require('gulp-uglify'),
 		concat = require('gulp-concat'),
 		scss = require('gulp-sass'),
-		cssnano = require('gulp-cssnano');
+		cssnano = require('gulp-cssnano'),
+		tape = require('gulp-tape'),
+		tapColorize = require('tap-colorize');
 
 var scssPath = './dev-assets/scss/**/*.scss',
 		jsPath = './dev-assets/js/modules/*.js',
@@ -48,16 +50,14 @@ gulp.task("build-prod-css", function () {
 // ========================================
 // Build JS Assets for Development
 gulp.task('build-dev-js', function() {
-	return gulp
-		.src(jsPath)
+	return gulp.src(jsPath)
 		.pipe(concat('custom.js'))
 		.pipe(gulp.dest(compiledJSPath));
 });
 
 // Build JS Assets for Production
 gulp.task('build-prod-js', function() {
-	return gulp
-		.src(jsPath)
+	return gulp.src(jsPath)
 		.pipe(concat('custom.js'))
 		.pipe(uglify())
 		.pipe(gulp.dest(compiledJSPath));
@@ -67,10 +67,10 @@ gulp.task('build-prod-js', function() {
 // Unit Tests
 // ========================================
 // Run Unit Tests
-gulp.task('unit-test', function() {
-	console.log('Running Unit Tests...');
-	// Test
-	console.log('Tests complete.');
+gulp.task('test', function() {
+	console.log("Running Unit Tests...");
+  return gulp.src('tests/*.js')
+    .pipe(tape({ reporter: tapColorize() }));
 });
 
 // ========================================
@@ -86,5 +86,5 @@ gulp.task('watch', function() {
 	// console.log(process.argv[2]);
 	// var environment = process.argv[2];
 	gulp.watch(scssWatchPath, ['build-dev-css']);
-	gulp.watch(jsWatchPath, ['unit-test', 'build-dev-js']);
+	gulp.watch(jsWatchPath, ['build-dev-js']);
 });
